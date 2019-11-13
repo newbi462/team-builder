a4394b05e30a01c2dc96a715fd467e57c6059d90
newbi462 <isthisemailfree@gmail.com>
2019-11-13 18:54:50 -0500

    SASS Works

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -28,29 +28,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
 
         <TeamMemberForm addTeamMembers={addTeamMembers}/>
 
 
-
-        {teamMembers.map(item => (
-          <div className="teamcard">
-            <p>{item.name}</p>
-            <p>{item.email}</p>
-            <p>{item.role}</p>
-          </div>
-        ))}
+        <div className="cardsDiv">
+          {teamMembers.map(item => (
+            <div className="teamcard">
+              <p>{item.name}</p>
+              <p>{item.email}</p>
+              <p>{item.role}</p>
+            </div>
+          ))}
+        </div>
 
       </header>
     </div>
