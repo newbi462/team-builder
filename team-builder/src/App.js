869ab599b4a0a4c48bf1fd9dc7d9e9379343d380
newbi462 <isthisemailfree@gmail.com>
2019-11-13 15:54:07 -0500

    Import the useState hook and set up a state property for your team members list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState( [] );
+
   return (
     <div className="App">
       <header className="App-header">
